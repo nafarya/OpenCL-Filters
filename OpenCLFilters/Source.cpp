a54c3003f4d003fb5fbf3c6779a3c7c0a8a1c8c1
nafarya <nafarya>
2018-02-22 19:10:52 +0300

    Add project files.

---
+++ OpenCLFilters/Source.cpp
@@ -0,0 +1,334 @@
+#include <CL/cl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <time.h>
+#include <omp.h>
+#include <io.h>
+
+using namespace std;
+
+const int KERNELSIZE = 5;
+
+const float FACTOR3x3 = 1;
+const float FACTOR5x5 = 13;
+
+float* blur3x3 = new float[9]{
+	0.0f, 0.2f, 0.0f,
+	0.2f, 0.2f, 0.2f,
+	0.0f, 0.2f, 0.0f
+};
+
+float* blur5x5 = new float[25]{
+	0, 0, 1, 0, 0,
+	0, 1, 1, 1, 0,
+	1, 1, 1, 1, 1,
+	0, 1, 1, 1, 0,
+	0, 0, 1, 0, 0
+};
+
+const int WS = 8;
+
+int cols, rows; //ONLY OF real image(not INCREASED)
+
+vector<vector<int>> rotate(vector<vector<int>> a, int window) {
+
+	//top
+	for (int i = 0; i < window / 2; i++) {
+		for (int j = 0; j < cols + window * 2; j++) {
+			swap(a[i][j], a[window - i - 1][j]);
+		}
+	}
+	//bottom
+	for (int i = rows + window; i < rows + window + window / 2; i++) {
+		for (int j = 0; j < cols + window * 2; j++) {
+			swap(a[i][j], a[rows + window * 2 - (window - ((rows + window * 2) - i)) - 1][j]);
+		}
+	}
+	//left
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = 0; j < window / 2; j++) {
+			swap(a[i][j], a[i][window - j - 1]);
+		}
+	}
+	//right
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = cols + window; j < cols + window + window / 2; j++) {
+			swap(a[i][j], a[i][cols + window * 2 - (window - ((cols + window * 2) - j)) - 1]);
+		}
+	}
+	return a;
+}
+
+vector<vector<int>> initIncreasedImg(vector<vector<int>> a, int window) {
+	//top
+	for (int i = 0; i < window; i++) {
+		for (int j = window; j < cols + window; j++) {
+			a[i][j] = a[i + window][j];
+		}
+	}
+	//bottom
+	for (int i = rows + window; i < rows + window * 2; i++) {
+		for (int j = window; j < cols + window; j++) {
+			a[i][j] = a[i - window][j];
+		}
+	}
+	//left
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = 0; j < window; j++) {
+			a[i][j] = a[i][j + window];
+		}
+	}
+	//right
+	for (int i = 0; i < rows + window * 2; i++) {
+		for (int j = cols + window; j < cols + window * 2; j++) {
+			a[i][j] = a[i][j - window];
+		}
+	}
+	//mirroring top corner
+	a = rotate(a, window);
+	return a;
+}
+
+enum state {
+	P5, DIM, MAXVAL, NUMS
+};
+
+vector<vector<int>>  readFile(const string& name) {
+	int w, h, mx, t;
+	int row, col;
+	vector<int> pixs;
+	ifstream infile(name);
+
+	string s;
+	state st = P5;
+
+	while (getline(infile, s)) {
+		if (s[0] == '#') continue;
+		istringstream ss(s);
+		switch (st) {
+		case P5:
+			st = DIM;
+			break;
+		case DIM:
+			ss >> w >> h;
+			cerr << "w:" << w << " h:" << h << endl;
+			rows = h, cols = w;
+			st = MAXVAL;
+			break;
+		case MAXVAL:
+			ss >> mx;
+			cerr << "mx:" << mx << endl;
+			st = NUMS;
+			break;
+		case NUMS:
+			while (ss >> t) {
+				pixs.push_back(t);
+			}
+			break;
+		}
+	}
+
+	//доводим до кратности 8
+	int curWindow = 8;
+	vector<vector<int>> pixels(h + curWindow * 2, vector<int>(w + curWindow * 2));
+	int cnt = 0;
+	for (row = curWindow; row < h + curWindow; row++) {
+		for (col = curWindow; col < w + curWindow; col++) {
+			pixels[row][col] = pixs[cnt++];
+		}
+	}
+	vector<vector<int>> tmp = initIncreasedImg(pixels, curWindow);
+
+	int hMulti = (h % WS == 0) ? 0 : WS - (h % WS);
+	int wMulti = (w % WS == 0) ? 0 : WS - (w % WS);
+	if (h % WS != 0 || w % WS != 0) {
+		printf("Image was increased due to multiplicity to WS=%d\nCurrent h:%d, w:%d\n\n", WS, h + hMulti, w + wMulti);
+	}
+	h += hMulti;
+	w += wMulti;
+	rows = h;
+	cols = w;
+
+
+	vector<vector<int>> pixels1(h + (curWindow * 2), vector<int>(w + (curWindow * 2)));
+	for (row = curWindow; row < h + curWindow; row++) {
+		for (col = curWindow; col < w + curWindow; col++) {
+			pixels1[row][col] = tmp[row][col];
+		}
+	}
+
+	//доводим до кратности 8
+
+	//int curWindow = 8;
+	//vector<vector<int>> pixels(h + curWindow * 2, vector<int>(w + curWindow * 2));
+	//int cnt = 0;
+	//for (row = curWindow; row < h + curWindow; row++) {
+	//	for (col = curWindow; col < w + curWindow; col++) {
+	//		pixels[row][col] = pixs[cnt++];
+	//	}
+	//}
+	return initIncreasedImg(pixels1, curWindow);
+}
+
+void err_check(int err, string err_code) {
+	if (err != CL_SUCCESS) {
+		cout << "Error: " << err_code << "(" << err << ")" << endl;
+		exit(-1);
+	}
+}
+
+void parallelFilterConv(float* a) {
+	cl_int error = 0;
+	cl_uint num_entries = 100;
+	cl_platform_id platforms[100];
+	cl_uint num_platforms = 0;
+	cl_int fn = clGetPlatformIDs(num_entries, platforms, &num_platforms);
+	printf("\n\nNumber of platforms = %d\n", num_platforms);
+
+	cl_device_type device_type = CL_DEVICE_TYPE_ALL;
+	cl_device_id devices[10];
+	cl_uint num_devices = 0;
+
+	cl_int n1 = clGetDeviceIDs(platforms[0], device_type, num_entries, devices, &num_devices);
+	printf("Number of devices = %d\n", num_devices);
+
+	char deviceNames[200];
+	printf("------------------------------------------\n");
+	for (int i = 0; i < num_devices; i++) {
+		cl_int aa = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 10240, deviceNames, NULL);
+		printf("%s\n", deviceNames);
+	}
+	printf("------------------------------------------\n\n");
+
+	
+	cl_context context = clCreateContext(NULL, num_devices, &devices[0], NULL, NULL, &error);
+	err_check(error, "Context status: Error\n");
+
+	cl_command_queue com_queue = clCreateCommandQueue(context, devices[0], CL_QUEUE_PROFILING_ENABLE, &error);
+	err_check(error, "clCreateCommandQueue: Error\n");
+
+
+	FILE * pFile;
+	size_t lSize;
+	char * buff;
+	size_t lengths;
+
+	pFile = fopen("blur_gaussian.cl", "rb");
+	if (pFile == NULL) {
+		fputs("File error", stderr);
+		exit(1);
+	}
+
+	lSize = _filelength(fileno(pFile));
+	buff = (char*)malloc(sizeof(char)*lSize);
+	if (buff == NULL) {
+		fputs("Memory error", stderr);
+		exit(2);
+	}
+
+	// copy the file into the buffer:
+	lengths = fread(buff, 1, lSize, pFile);
+	if (lengths != lSize) { fputs("Reading error", stderr); exit(3); }
+
+	fclose(pFile);
+	const char *strings = const_cast<char*>(buff);
+	cl_program prog = clCreateProgramWithSource(context, 1, &strings, &lSize, &error);
+
+	cl_int builder = clBuildProgram(prog, 1, &devices[0], "", NULL, NULL);
+
+	char buffer[2048];
+	error = clGetProgramBuildInfo(prog, devices[0], CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, NULL);
+	printf("%s\n", buffer);
+
+	cl_image_format img_fmt;
+	img_fmt.image_channel_order = CL_R;
+	img_fmt.image_channel_data_type = CL_FLOAT;
+	cl_mem image1, image3;
+	size_t width = (cols + WS * 2), height = rows + WS * 2;
+	image1 = clCreateImage2D(context, CL_MEM_READ_ONLY, &img_fmt, width, height, 0, 0, &error);
+	err_check(error, "image1: clCreateImage2D");
+	image3 = clCreateImage2D(context, CL_MEM_READ_WRITE, &img_fmt, width, height, 0, 0, &error);
+	err_check(error, "image3: clCreateImage2D");
+
+
+	const char *kernel_name = "gaussian_blur_local";
+	cl_kernel ker = clCreateKernel(prog, kernel_name, &error);
+
+
+	cl_mem buff_filter = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float)*KERNELSIZE*KERNELSIZE, NULL, &error);
+
+	clSetKernelArg(ker, 0, sizeof(image1), &image1);
+	clSetKernelArg(ker, 1, sizeof(image3), &image3);
+	clSetKernelArg(ker, 2, sizeof(buff_filter), &buff_filter);
+	clSetKernelArg(ker, 3, sizeof(int), &KERNELSIZE);
+	clSetKernelArg(ker, 4, sizeof(float), &FACTOR5x5);
+
+	clEnqueueWriteBuffer(com_queue, buff_filter, CL_FALSE, 0, sizeof(float)*KERNELSIZE*KERNELSIZE, blur5x5, NULL, NULL, NULL);
+
+	size_t origin[] = { 0,0,0 }; // Defines the offset in pixels in the image from where to write.
+	size_t region[] = { width, height, 1 }; // Size of object to be transferred
+	error = clEnqueueWriteImage(com_queue, image1, CL_TRUE, origin, region, 0, 0, a, 0, NULL, NULL);
+
+
+	const size_t global_work_size[] = { cols, rows, 1 };
+	const size_t local_work_size[] = { WS, WS };
+
+
+	cl_event timer;
+	error = clEnqueueNDRangeKernel(com_queue, ker, 2, 0, global_work_size, local_work_size, NULL, NULL, &timer);
+
+
+	if (error == 0) {
+		printf("Kernel execution was successfull\n\n");
+	} else {
+		printf("Kernel ERROR%d\n\n", error);
+	}
+
+	clFinish(com_queue);
+	cl_ulong start, end;
+	clGetEventProfilingInfo(timer, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start, NULL);
+	clGetEventProfilingInfo(timer, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end, NULL);
+	printf("time: %llu\n", end - start);
+
+
+
+	size_t origin1[] = { WS,WS,0 }; 
+	size_t region1[] = { cols, rows, 1 }; 
+
+	vector<float> output(cols * rows);
+	clEnqueueReadImage(com_queue, image3, CL_TRUE, origin1, region1, 0, 0, output.data(), NULL, NULL, NULL);
+
+
+	ofstream out("blured.pgm");
+	out << "P2\n" << cols << ' ' << rows << "\n255\n";
+	for (int i = 0; i < cols*rows; i++) {
+		out << output[i] << '\n';
+	}
+	out.close();
+
+	clReleaseProgram(prog);
+}
+
+int main() {
+
+
+	vector<vector<int>> file1 = readFile("frame0015.pgm");
+
+	float* a = new float[(rows + WS * 2)*(cols + WS * 2) * 3];
+
+	int cnt = 0;
+	for (int i = 0; i < rows + WS * 2; i++) {
+		for (int j = 0; j < cols + WS * 2; j++) {
+			a[cnt] = (float)file1[i][j];
+			cnt++;
+		}
+	}
+
+	parallelFilterConv(a);
+	system("pause");
+	return 0;
+}
---
+++ OpenCLFilters/blur_gaussian.cl
@@ -0,0 +1,76 @@
+__constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
+
+
+__kernel void gaussian_blur_local(__read_only image2d_t src, __write_only image2d_t output,  __constant float *convArr, int convSize, float factor) {
+	
+	
+	const int WS = 8;
+	const int X = get_group_id(0);
+	const int Y = get_group_id(1);
+
+	const int BLOCK_X = X * WS + WS;
+	const int BLOCK_Y = Y * WS + WS;
+	
+	const int f  =  get_local_id(0);
+	const int f1 =  get_local_id(1);
+
+	const int HALFCONVSZ = (convSize / 2);
+	
+	__local float4 B[WS*3][WS*3];
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {	
+			B[(i+1)*WS+f][(j+1)*WS+f1] = read_imagef(src, sampler, (int2)(BLOCK_X+f1 + (j*WS), BLOCK_Y+f + (i*WS)));
+		}
+	}  
+	barrier(CLK_LOCAL_MEM_FENCE);
+	
+	
+	for (int i = 0; i < WS; i++) {
+		for (int j = 0; j < WS; j++) {
+			float4 convPix = (float4)(0.0f, 0.0f, 0.0f, 0.0f);
+			for (int conv_i = -HALFCONVSZ; conv_i <= HALFCONVSZ; conv_i++) {
+					for (int conv_j = -HALFCONVSZ; conv_j <= HALFCONVSZ; conv_j++) {
+						float4 px = B[i+conv_i+WS][j+conv_j+WS];
+						convPix += px * convArr[(conv_i+1)*convSize+(conv_j+1)];
+					}
+				}
+			barrier(CLK_LOCAL_MEM_FENCE);
+			write_imagef(output, (int2)(BLOCK_X+j, BLOCK_Y+i), convPix / factor);
+		}
+	}
+
+
+	
+}
+
+
+
+__kernel void gaussian_blur_global(__read_only image2d_t src, __write_only image2d_t output,  __constant float *convArr, int convSize, float factor) {
+	const int BLOCKSIZE = 8;
+	const int X = get_global_id(0);
+	const int Y = get_global_id(1);
+
+	const int BLOCK_X = X * BLOCKSIZE + BLOCKSIZE;
+	const int BLOCK_Y = Y * BLOCKSIZE + BLOCKSIZE;
+
+	const int HALFCONVSZ = (convSize / 2);
+
+	for (int i = 0; i < BLOCKSIZE; i++) {
+		for (int j = 0; j < BLOCKSIZE; j++) {
+				float4 convPix = (float4)(0.0f, 0.0f, 0.0f, 0.0f);
+				for (int conv_i = -HALFCONVSZ; conv_i <= HALFCONVSZ; conv_i++) {
+					for (int conv_j = -HALFCONVSZ; conv_j <= HALFCONVSZ; conv_j++) {
+						float4 px = read_imagef(src, sampler, (int2)(BLOCK_X+i+conv_j, BLOCK_Y+j+conv_i));
+						convPix += px * convArr[(conv_i+1)*convSize+(conv_j+1)];
+					}
+				}
+				write_imagef(output, (int2)(BLOCK_X+i, BLOCK_Y+j), convPix / factor);		
+		}
+	}
+}
+
+
+
+
+
+
